95b4501bb2661c3238c4ecfbc6b118b9a79b2b84
Kirill <kirillsavorin@gmail.com>
2024-11-25 23:51:11 +0100

    JSX

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,26 @@ import { createElement, useState } from 'react'
 import ChallengeList from './challengeList'
 
 function App() {
+  // return (
+  //   createElement('div', { className: 'foo'},
+  //     createElement('button', {}, 'Only A + B'),
+  //     createElement('button', {}, 'B'),
+  //     createElement('button', {}, 'All'),
+  //     createElement('p', {}, 'Showing 3'),
+  //     createElement(ChallengeList, {}),
+  //     )
+  //   )
+
+  //JSX
   return (
-    createElement('div', { className: 'foo'},
-      createElement('button', {}, 'Only A + B'),
-      createElement('button', {}, 'B'),
-      createElement('button', {}, 'All'),
-      createElement('p', {}, 'Showing 3'),
-      createElement(ChallengeList, {}),
-      )
-    )
+    <div className='container'>
+      <button>Only A+B</button>
+      <button>Only B</button>
+      <button>All</button>
+      <p>Showing 3</p>
+      <ChallengeList/>
+    </div>
+  );
 }
 
 export default App
--- src/components/ChallengeList.jsx
+++ src/components/ChallengeList.jsx
@@ -1,13 +1,22 @@
 import { createElement } from "react"
 
 function ChallengeList() {
+  // return (
+  //   createElement('ul', {},
+  //     createElement('li', {}, 'Challenge A'),
+  //     createElement('li', {}, 'Challenge B'),
+  //     createElement('li', {}, 'Challenge C'),
+  //   )
+  // )
+
+  // JSX
   return (
-    createElement('ul', {},
-      createElement('li', {}, 'Challenge A'),
-      createElement('li', {}, 'Challenge B'),
-      createElement('li', {}, 'Challenge C'),
-    )
-  )
+    <ul>
+      <li>Challenge A</li>
+      <li>Challenge B</li>
+      <li>Challenge C</li>
+    </ul>
+  );
 }
 
 export default ChallengeList
